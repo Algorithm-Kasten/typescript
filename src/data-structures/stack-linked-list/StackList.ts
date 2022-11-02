03ec9d10dadd8f72812d16b89e9ea0a1e09c5207
Jii Eu <hello@jiieu.com>
2022-11-03 04:20:38 +0900

    stack updated

--- src/data-structures/stack-linked-list/StackList.ts
+++ src/data-structures/stack-linked-list/StackList.ts
@@ -11,29 +11,16 @@ export class StackList<T> {
     }
   }
 
-  /**
-   * Returns true if stack is empty else false.
-   * @returns boolean
-   */
   empty(): boolean {
     return this.top === null;
   }
 
-  /**
-   * Add an element to the top of a stack.
-   * @param val - T
-   * @returns void
-   */
   push(value: T): void {
     let newNode = new LinkedListNode(value);
     newNode.next = this.top;
     this.top = newNode;
   }
 
-  /**
-   * Remove and return the top element in the stack. Returns _undefined_ if stack is empty.
-   * @returns ListNode<T>
-   */
   pop(): ListNode<T> {
     let poppedNode = this.top;
 
@@ -44,10 +31,6 @@ export class StackList<T> {
     return poppedNode;
   }
 
-  /**
-   * Get the value of the top element without removing it.
-   * @returns ListNode<T>
-   */
   peek(): ListNode<T> {
     if (this.top) {
       return this.top;

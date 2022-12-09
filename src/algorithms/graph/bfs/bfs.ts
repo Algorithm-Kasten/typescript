b2ce7096c2ef8ff1a1740c7cb674f445bf471f69
bprsstnt <bprsstnt@pm.me>
2022-12-09 13:28:35 +0900

    bfs code modified

--- src/algorithms/graph/bfs/bfs.ts
+++ src/algorithms/graph/bfs/bfs.ts
@@ -1,45 +1,32 @@
 import { AdjacencyList } from "../../../data-structures/adjacency-list/AdjacencyList";
 
-const bfs = (startingNode: number, target: number): boolean => {
+const bfs = (startingNode: number): void => {
     let q: number[] = [startingNode];
     let visited: boolean[] = new Array(graph.vertex).fill(false);
+    visited[startingNode] = true;
 
-    while (q.length !== 0) {
+    while (q.length > 0) {
         let v: number = q.shift() || 0;
+        process.stdout.write(`${v} `);
 
         let node = graph.list[v]?.getNode(1);
         while (node != null) {
-            if (visited[node.value] !== true) {
-                if (node.value === target) {
-                    return true;
-                }
-
-                q.push(node.value);
+            if (!visited[node.value]) {
                 visited[node.value] = true;
+                q.push(node.value);
             }
             node = node.next;
         }
-
     }
-
-    return false;
 };
 
 let graph = new AdjacencyList(4);
-graph.addEdge(0, 2);
-graph.addEdge(0, 3);
-graph.addEdge(1, 3);
-graph.addEdge(2, 1);
-graph.addEdge(2, 0);
-graph.addEdge(3, 0);
-graph.addEdge(3, 2);
+const IS_UNDIRECTED = false;
+graph.addEdge(0, 1, IS_UNDIRECTED);
+graph.addEdge(0, 2, IS_UNDIRECTED);
+graph.addEdge(1, 2, IS_UNDIRECTED);
+graph.addEdge(2, 0, IS_UNDIRECTED);
+graph.addEdge(2, 3, IS_UNDIRECTED);
+graph.addEdge(3, 3, IS_UNDIRECTED);
 
-
-const NODE_TO_FIND = 3;
-let doesExist = bfs(0, NODE_TO_FIND);
-
-if(doesExist) {
-    console.log(`${NODE_TO_FIND} exists in the graph.`);
-} else {
-    console.log(`${NODE_TO_FIND} does not exist.`);
-}
+bfs(2); // 2 0 3 1

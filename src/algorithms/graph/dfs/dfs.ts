22a0ceea628cbb0da443821b154bf26ef32038b0
bprsstnt <bprsstnt@pm.me>
2022-12-09 15:01:04 +0900

    graph DFS code added

---
+++ src/algorithms/graph/dfs/dfs.ts
@@ -0,0 +1,32 @@
+import { AdjacencyList } from "../../../data-structures/adjacency-list/AdjacencyList";
+
+const dfs = (startingNode: number): void => {
+    let stack: number[] = [startingNode];
+    let visited: boolean[] = new Array(graph.vertex).fill(false);
+    visited[startingNode] = true;
+
+    while (stack.length > 0) {
+        let v: number = stack.pop() || 0;
+        process.stdout.write(`${v} `);
+
+        let node = graph.list[v]?.getNode(1);
+        while (node != null) {
+            if (!visited[node.value]) {
+                visited[node.value] = true;
+                stack.push(node.value);
+            }
+            node = node.next;
+        }
+    }
+};
+
+let graph = new AdjacencyList(6);
+const IS_UNDIRECTED = false;
+graph.addEdge(1, 2, IS_UNDIRECTED);
+graph.addEdge(1, 3, IS_UNDIRECTED);
+graph.addEdge(2, 4, IS_UNDIRECTED);
+graph.addEdge(2, 5, IS_UNDIRECTED);
+
+console.dir(graph.list[1]);
+
+dfs(1);

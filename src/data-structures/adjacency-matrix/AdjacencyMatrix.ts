199ec343003710a09d5db500491257b15beb3e29
Jii Eu <hello@jiieu.com>
2022-12-09 10:13:25 +0900

    adjacency list and matrix code modified

--- src/data-structures/adjacency-matrix/AdjacencyMatrix.ts
+++ src/data-structures/adjacency-matrix/AdjacencyMatrix.ts
@@ -1,31 +1,29 @@
-import process from "process";
-
 export class AdjacencyMatrix {
     vertex: number;
-    graph: number[][] | null[][];
+    matrix: number[][] | null[][];
     constructor(v: number) {
         this.vertex = v;
-        this.graph = new Array(v);
+        this.matrix = new Array(v);
         for (let i = 0; i < v; ++i) {
-            this.graph[i] = new Array(v).fill(null);
+            this.matrix[i] = new Array(v).fill(null);
         }
     }
 
     addEdge(u: number, v: number) {
-        this.graph[u][v] = 1;
-        this.graph[v][u] = 1;
+        this.matrix[u][v] = 1;
+        this.matrix[v][u] = 1;
     }
 
     removeEdge(u: number, v: number) {
-        this.graph[u][v] = null;
-        this.graph[v][u] = null;
+        this.matrix[u][v] = null;
+        this.matrix[v][u] = null;
     }
 
     print() {
         for (let i = 0; i < this.vertex; ++i) {
             process.stdout.write(`${i}: `);
             for (let j = 0; j < this.vertex; ++j) {
-                if (this.graph[i][j]) {
+                if (this.matrix[i][j]) {
                     process.stdout.write(`${j} → `);
                 }
             }
@@ -33,15 +31,3 @@ export class AdjacencyMatrix {
         }
     }
 }
-
-let graph = new AdjacencyMatrix(5);
-graph.addEdge(0, 1);
-graph.addEdge(0, 2);
-graph.addEdge(1, 4);
-graph.addEdge(2, 3);
-
-graph.print();
-
-console.log();
-graph.removeEdge(1, 0);
-graph.print();

5cee5383dca722c752be13e603cfd7fd0a13cb0f
bprsstnt <bprsstnt@pm.me>
2022-12-09 10:13:25 +0900

    adjacency list and matrix code modified

--- src/data-structures/adjacency-list/AdjacencyList.ts
+++ src/data-structures/adjacency-list/AdjacencyList.ts
@@ -1,45 +1,32 @@
-import process from "process";
 import { LinkedList } from '../linked-list/LinkedList';
 
 export class AdjacencyList {
     vertex: number;
-    graph: LinkedList<number>[];
+    list: LinkedList<number>[];
     constructor(v: number) {
         this.vertex = v;
-        this.graph = new Array(v);
+        this.list = new Array(v);
         for (let i = 0; i < v; ++i) {
-            this.graph[i] = new LinkedList<number>();
+            this.list[i] = new LinkedList<number>();
         }
     }
 
     addEdge(u: number, v: number) {
-        this.graph[u].append(v);
-        this.graph[v].append(u);
+        this.list[u].append(v);
+        this.list[v].append(u);
     }
 
     removeEdge(u: number, v: number) {
-        if (this.graph[u].size() > 0) {
-            this.graph[u].removeValue(v);
-            this.graph[v].removeValue(u);
+        if (this.list[u].size() > 0) {
+            this.list[u].removeValue(v);
+            this.list[v].removeValue(u);
         }
     }
 
     print() {
         for (let i = 0; i < this.vertex; ++i) {
             process.stdout.write(`${i}: `)
-            this.graph[i].print();
+            this.list[i].print();
         }
     }
 }
-
-let graph = new AdjacencyList(5);
-graph.addEdge(0, 1);
-graph.addEdge(0, 2);
-graph.addEdge(1, 4);
-graph.addEdge(2, 3);
-
-graph.print();
-
-console.log();
-graph.removeEdge(1, 0);
-graph.print();

ddd1c6660a16970b1831f357403102fa36f94de1
bprsstnt <bprsstnt@pm.me>
2023-03-19 14:53:38 -0400

    linked list updated

--- src/data-structures/linked-list/LinkedList.ts
+++ src/data-structures/linked-list/LinkedList.ts
@@ -1,220 +1,198 @@
+const ListDNEError = new Error('list does not exist.');
+const ListEmptyError = new Error('list is empty.');
+const IndexOutOfBoundsError = new Error('index is out of bounds.');
+
 export type ListNode<T> = LinkedListNode<T> | null;
 
 export class LinkedListNode<T> {
-  readonly value: T;
+  readonly item: T;
   next: ListNode<T>;
-
-  /**
-   * @param value - readonly number
-   * @param next
-   */
-  constructor(value: T, next: ListNode<T> = null) {
-    this.value = value;
-    this.next = next;
+  
+  constructor(item: T) {
+    this.item = item;
+    this.next = null;
   }
 }
 
-export class LinkedList<T> {
-  private count: number;
-  head: ListNode<T>;
-  tail: ListNode<T>;
+export class SinglyLinkedList<T> {
+  sentinel: ListNode<T>;
+  node_count: number;
 
-  constructor() {
-    this.count = 0;
-    this.head = null;
-    this.tail = null;
+  constructor(item: T) {
+    this.sentinel = new LinkedListNode(item);
+    this.node_count = 0;
   }
 
-  /**
-   * @returns number - a total number of nodes in the LinkedList
-   */
-  size(): number {
-    return this.count;
+  size() {
+    return this.node_count;
   }
 
-  /**
-   * @returns boolean
-   */
-  empty(): boolean {
-    return this.count === 0;
+  empty() {
+    return this.node_count === 0;
   }
 
-  /**
-   * @param index - number (1-based)
-   * @returns ListNode
-   */
-  getNode(index: number): ListNode<T> {
-    let curr: ListNode<T> = this.head;
+  // one-based index
+  getNode(index: number) {
+    if (index <= 0 || index > this.node_count) {
+      throw IndexOutOfBoundsError;
+    }
 
-    let i = 1;
-    while (curr && i < index) {
-      curr = curr.next;
-      --index;
+    let curr: ListNode<T> = this.sentinel;
+    for (let i = 0; i < index; ++i) {
+      if(curr) {
+        curr = curr.next;
+      }
     }
 
     return curr;
   }
 
-  /**
-   * @param value - number
-   * @returns void
-   */
-  prepend(value: T): void {
-    let newNode = new LinkedListNode<T>(value, this.head);
-    this.head = newNode;
-
-    if (null === this.tail) {
-      this.tail = newNode;
+  prepend(item: T) {
+    if(null === this.sentinel) {
+      this.sentinel = new LinkedListNode(item);
+      this.sentinel.next = new LinkedListNode(item);
+    } else { 
+      let newNode = new LinkedListNode(item);
+      newNode.next = this.sentinel.next;
+      this.sentinel.next = newNode;
     }
-
-    this.count += 1;
+    this.node_count += 1;
   }
 
-  /**
-   * @returns ListNode
-   */
-  deleteHead(): ListNode<T> {
-    if (this.head === null) {
-      return null;
+  deleteHead() {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
     }
 
-    let deletedNode = this.head;
-
-    if (this.count === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      this.head = this.head.next;
-    }
-
-    this.count -= 1;
-    return deletedNode;
-  }
-
-  /**
-   * @param value - number
-   * @returns void
-   */
-  append(value: T): void {
-    let newNode = new LinkedListNode<T>(value);
-
-    if (null === this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail!.next = newNode;
-      this.tail = newNode;
-    }
-
-    this.count += 1;
-  }
-
-  /**
-   * @returns ListNode
-   */
-  deleteTail(): ListNode<T> {
-    if (this.head === null) {
-      return null;
-    }
-
-    let deletedNode = this.head;
-
-    if (this.count === 1) {
-      this.head = null;
-      this.tail = null;
-    } else {
-      let curr = this.head;
-      while (curr.next) {
-        if (null === curr.next.next) {
-          deletedNode = curr.next;
-          curr.next = null;
-        } else {
-          curr = curr.next;
-        }
-      }
-      this.tail = curr;
-    }
-
-    this.count -= 1;
-    return deletedNode;
-  }
-
-  /**
-   * Returns the front item.
-   * @returns ListNode
-   */
-  front(): ListNode<T> {
-    return this.head;
-  }
-
-  /**
-   * Returns the last item.
-   * @returns ListNode
-   */
-  back(): ListNode<T> {
-    return this.tail;
-  }
-
-  /**
-   * @param value - number
-   * @param index - number (0-based)
-   */
-  insert(value: T, index: number): void {
-    index = index < 0 ? 0 : index > this.count ? this.count : index;
-
-    if (index === 0) {
-      this.prepend(value);
-    } else if (index === this.count) {
-      this.append(value);
-    } else {
-      let curr: ListNode<T> = this.head;
-      const newNode = new LinkedListNode<T>(value);
-
-      while (curr && index > 1) {
-        curr = curr.next;
-        --index;
-      }
-
-      newNode.next = curr!.next;
-      curr!.next = newNode;
-      this.count += 1;
+    if(this.sentinel.next) {
+      this.sentinel.next = this.sentinel.next.next;
+      this.node_count -= 1;
     }
   }
 
-  /**
-   * Removes node at a given index.
-   *
-   * @param index - 0-based
-   * @returns ListNode
-   */
-  erase(index: number): ListNode<T> {
-    if (index === 0) {
-      return this.deleteHead();
-    } else if (index === this.count - 1) {
-      return this.deleteTail();
-    } else {
-      let deletedNode: ListNode<T> = null;
-      let curr: ListNode<T> = this.head;
+  deleteTail() {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
+    }
 
-      for (let i = 1; curr && i < index; ++i) {
+    let curr = this.sentinel.next;
+
+    while(curr && curr.next && curr.next.next) {
+      curr = curr.next;
+    }
+
+    this.node_count -= 1;
+    curr.next = null;
+  }
+
+  getFront() {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
+    }
+
+    return this.sentinel!.next?.item;
+  }
+  
+  getBack() {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
+    }
+
+    let curr = this.sentinel.next;
+    while(curr && curr.next) {
+      curr = curr.next;
+    }
+
+    return curr?.item;
+  }
+
+  // one-based index
+  insert(item: T, index: number) {
+    if(this.empty()) {
+      this.prepend(item);
+      return;
+    }
+
+    if (index <= 0 || index > this.node_count + 1) {
+      throw IndexOutOfBoundsError;
+    }
+
+    let curr: ListNode<T> = this.sentinel;
+
+    while(curr && curr.next && index > 1) {
+      curr = curr.next;
+      index -= 1;
+    }
+
+    let newNode = new LinkedListNode(item);
+
+    if(curr) {
+      newNode.next = curr.next;
+      curr.next = newNode;
+    }
+
+    this.node_count += 1;
+  }
+  
+  // one-based index
+  erase(index: number) {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
+    }
+
+    if(index <= 0 || index > this.node_count) {
+      throw IndexOutOfBoundsError;
+    }
+
+    if(1 === index) {
+      this.deleteHead();
+      return;
+    }
+
+    if(this.node_count === index) {
+      this.deleteTail();
+      return;
+    }
+
+    let curr: ListNode<T> = this.sentinel;
+    for(let i = 1; i < index; ++i) {
+      if(curr) {
         curr = curr.next;
       }
+    }
 
-      if (curr?.next) {
-        deletedNode = curr.next;
-        curr.next = curr.next.next;
-      }
-
-      this.count -= 1;
-      return deletedNode;
+    if(curr && curr.next) {
+      curr.next = curr.next.next;
+      this.node_count -= 1;
     }
   }
 
-  /**
-   * Reverse the list
-   */
   reverseIter(): void {
-    let curr: ListNode<T> = this.head;
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+
+    let curr: ListNode<T> = this.sentinel.next;
     let prev: ListNode<T> = null;
     let temp: ListNode<T> = null;
 
@@ -226,21 +204,15 @@ export class LinkedList<T> {
     }
     if (curr) curr.next = prev;
 
-    this.tail = this.head;
-    this.head = curr;
+    this.sentinel.next = curr;
   }
 
-  /**
-   * Reverse the list
-   * @params node - ListNode
-   * @returns ListNode
-   */
-  reverseRecur(node: ListNode<T> = this.head): ListNode<T> {
+  reverseRecur(node: ListNode<T> = this.sentinel!.next): ListNode<T> {
     if (null === node) {
       return null;
     }
     if (null === node?.next) {
-      this.head = node;
+      this.sentinel!.next = node;
       return node;
     }
 
@@ -250,68 +222,65 @@ export class LinkedList<T> {
     return node;
   }
 
-  /**
-   * @param value - number
-   * @returns ListNode
-   */
-  removeValue(value: T): ListNode<T> {
-    let deletedNode: ListNode<T> = null;
 
-    if (null === this.head) {
-      return null;
-    } else if (value === this.head.value) {
-      deletedNode = this.head;
-      this.head = this.head.next;
-    } else {
-      let curr: ListNode<T> = this.head;
-
-      while (curr?.next) {
-        if (curr.next.value === value) {
-          deletedNode = curr.next;
-          curr.next = curr.next.next;
-          if (curr.next === this.tail) {
-            this.tail = curr;
-          }
-        }
-
-        curr = curr.next;
-      }
+  removeItem(item: T) {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
     }
 
-    this.count -= 1;
-    return deletedNode;
-  }
+    let curr: ListNode<T> = this.sentinel;
 
-  /**
-   * @param value - number
-   * @returns ListNode
-   */
-  find(value: T): ListNode<T> {
-    let curr: ListNode<T> = this.head;
-
-    while (curr) {
-      if (curr.value === value) {
-        return curr;
+    while(curr && curr.next) {
+      if(curr.next.item === item) {
+        curr.next = curr.next.next;
+        this.node_count -= 1;
+        return;
       }
-
       curr = curr.next;
     }
+  }
+
+  find(item: T): ListNode<T> {
+    if(null === this.sentinel) {
+      throw ListDNEError;
+    }
+    
+    if(null === this.sentinel.next) {
+      throw ListEmptyError;
+    }
+
+    let curr: ListNode<T> = this.sentinel;
+    while(curr) {
+      if(curr.item === item) {
+        return curr;
+      }
+      curr = curr.next;
+    }
+
     return null;
   }
 
-  print(): void {
-    if (this.empty()) {
-      console.error('list is empty');
-      return;
-    }
-    let curr: ListNode<T> = this.head;
-    let str: string = '';
 
-    while (curr) {
-      str += `${curr.value} `;
-      curr = curr.next;
-    }
+  print() {
+    if(this.sentinel) {
+      let curr = this.sentinel.next;
+      let str = '';
+      while(curr) {
+        str += `${curr.item} `;
+        curr = curr.next;
+      }
 
-    console.log(str);
+      console.log(str);
+      console.log("Is empty?: " + this.empty());
+      console.log("Node count: " + this.node_count);
+      if(this.node_count > 0) {
+        console.log(`front: ${this.getFront()}    back: ${this.getBack()}`);
+      }
+      console.log();
+    }
   }
 }
